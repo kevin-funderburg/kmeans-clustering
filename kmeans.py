1ec414c294f94775953350e499ab25cda5cf31ad
Kevin Funderburg <ksfunderburg@icloud.com>
2020-03-05 12:06:36 -0600

    looping through inputs creating outputs

--- kmeans.py
+++ kmeans.py
@@ -4,7 +4,6 @@ import numpy as np
 import math
 import argparse
 
-centroids = []
 DATA_SET = []
 k = None
 MAX_ITERATIONS = 500
@@ -13,60 +12,81 @@ MAX_ITERATIONS = 500
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-k', dest='k_input', nargs='?', default=None)
+    # parser.add_argument('-a', dest='all', nargs='?', default=None)
     args = parser.parse_args()
 
+    # if not args.all:
     if not args.k_input:
         print("k wasn't provided, try again!")
         return 0
 
     if args.k_input:
+        files = os.listdir("./output")
+        for file in files:
+            os.remove("./output/" + file)
+
+        # if args.k_input == 'all':
+
         k_input = int(args.k_input)
         if k_input <= 1:
             print("k must be > 1, try again!")
             return 0
+    # else:
 
-    read_data()
+    inputFiles = os.listdir("./data")
 
-    # pick K points as the initial centroids
-    for n in range(k):
-        centroids.append(DATA_SET[n])
+    for input in inputFiles:
+        global k
+        global DATA_SET
+        centroids = []
 
-    print('k = ' + str(k))
+        k, DATA_SET = read_data(input)
 
-    # start k-mean clustering
-    for n in range(MAX_ITERATIONS):
-        classes = {}
-        for i in range(k):
-            classes[i] = []
+        # pick K points as the initial centroids
+        for x in range(k):
+            centroids.append(DATA_SET[x])
 
-        # find the distance between the points and the centroids
-        for point in DATA_SET:
-            distances = []
+        print('k = ' + str(k))
 
-            for index in centroids:
-                distances.append(euclidean_distance(point, index))
+        # begin k-mean clustering
+        for n in range(MAX_ITERATIONS):
+            classes = {}
+            for i in range(k):
+                classes[i] = []
 
-            # find which cluster the datapoint belongs to by finding the minimum
-            cluster_index = distances.index(min(distances))
-            classes[cluster_index].append(point)
+            # find the distance between the points and the centroids
+            for point in DATA_SET:
+                distances = []
 
-            if n == MAX_ITERATIONS-1:
-                print("{0}\t{1}\t{2}".format(point[0], point[1], cluster_index+1))
+                for index in centroids:
+                    distances.append(euclidean_distance(point, index))
 
-        # now that we have classified the datapoints into clusters, we need to again
-        # find new centroid by taking the centroid of the points in the cluster class
-        for cluster_index in classes:
-            centroids[cluster_index] = np.average(classes[cluster_index], axis=0)
+                # find which cluster the datapoint belongs to by finding the minimum
+                cluster_index = distances.index(min(distances))
+                classes[cluster_index].append(point)
 
-    print('done')
+                if n == MAX_ITERATIONS-1:
+                    out_fname = input.replace("input", "output")
+                    output = open("./output/" + out_fname, "a")
+                    output.write("{0}\t{1}\t{2}\r".format(point[0], point[1], cluster_index+1))
+                    print("{0}\t{1}\t{2}".format(point[0], point[1], cluster_index+1))
+
+            # now that we have classified the datapoints into clusters, we need to again
+            # find new centroid by taking the centroid of the points in the cluster class
+            for cluster_index in classes:
+                centroids[cluster_index] = np.average(classes[cluster_index], axis=0)
+
+        print('done')
 
 
-def read_data():
-    global k
-    input1 = open('./data/input1.txt', 'r')
+def read_data(fname):
+    k = None
+    dataset = []
+
+    input = open('./data/' + fname, 'r')
 
     # get the data from the input file
-    for line in input1:
+    for line in input:
         line = line.strip()
         if "k=" in line:
             k = int(line.split("=")[1])
@@ -78,8 +98,9 @@ def read_data():
                 else:
                     y = int(items[n])
 
-            DATA_SET.append([x,y])
+            dataset.append([x,y])
 
+    return k, dataset
 
 def euclidean_distance(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
